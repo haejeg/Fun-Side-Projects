7b71d36674bfac2f677a25396241a59f919314d1
haejeg <88898962+haejeg@users.noreply.github.com>
2024-05-17 23:02:08 -0600

    Create Roblox_Gun.lua
    Conditional Headshot Damage

---
+++ Roblox_Gun.lua
@@ -0,0 +1,15 @@
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+
+script.Parent.Activated:Connect(function()
+	local target = mouse.Target
+	if target and target.Parent:FindFirstChild("Humanoid") then
+		if target.Name == "Head" then
+			print("Headshot")
+			script.Parent.RemoteEvent:FireServer(target.Parent, 100)
+		else
+			print("-10")
+		script.Parent.RemoteEvent:FireServer(target.Parent, 10)
+		end
+	end
+end)
