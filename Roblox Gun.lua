d1485b3e76385dfb60f02f5084e5b9049f3f4e00
haejeg <88898962+haejeg@users.noreply.github.com>
2024-05-18 00:05:49 -0600

    Funny looking recoil, sound, and muzzle flash

--- Roblox Gun.lua
+++ Roblox Gun.lua
@@ -3,6 +3,13 @@ local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
 
 script.Parent.Activated:Connect(function()
+	print("Firing")
+	local handle = script.Parent.Handle
+	local currentOrientation = handle.Orientation
+	handle.Orientation = Vector3.new(currentOrientation.X  + 10, currentOrientation.Y, currentOrientation.Z)
+	script["half life 2 usp shoot"]:Play()
+	script.Parent.Handle.MuzzleFlash.Enabled = true
+
 	local target = mouse.Target
 	if target and target.Parent:FindFirstChild("Humanoid") then
 		if target.Name == "Head" then
@@ -10,7 +17,14 @@ script.Parent.Activated:Connect(function()
 			script.Parent.RemoteEvent:FireServer(target.Parent, 100)
 		else
 			print("-10")
-		script.Parent.RemoteEvent:FireServer(target.Parent, 10)
+			script.Parent.RemoteEvent:FireServer(target.Parent, 10)
 		end
 	end
+	
+	wait(0.1)
+	script.Parent.Handle.MuzzleFlash.Enabled = false
+	wait(0.3)
+	currentOrientation = handle.Orientation
+	handle.Orientation = Vector3.new(currentOrientation.X - 10, currentOrientation.Y, currentOrientation.Z)
+	
 end)
